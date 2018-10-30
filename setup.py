64e90259967f8873603403537d273864ce75dc40
Michal Čihař <michal@cihar.com>
2018-10-30 16:31:28 +0100

    Initial setup
    Signed-off-by: Michal Čihař <michal@cihar.com>

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import os
+import sys
+from setuptools import setup
+
+setup(
+    name="fakturace",
+    version="0.1",
+    author="Michal Čihař",
+    author_email="michal@cihar.com",
+    description="A translation finder for Weblate, translation tool with tight version control integration",
+    license="MIT",
+    url="https://cihar.com/",
+    download_url="https://github.com/nijel/fakturace",
+    platforms=["any"],
+    packages=["fakturace"],
+    package_dir={"fakturace": "fakturace"},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+    ],
+    entry_points={
+        "console_scripts": ["fakturace = fakturace.cli:main"]
+    },
+)
