a52115c9e64f73199d640ae3044970d069df9510
Michal Čihař <michal@cihar.com>
2020-07-07 11:34:06 +0200

    Drop support for older Python versions

--- setup.py
+++ setup.py
@@ -32,12 +32,10 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Topic :: Utilities",
     ],
     entry_points={"console_scripts": ["fakturace = fakturace.cli:main"]},
-    python_requires=">=3.5",
+    python_requires=">=3.7",
 )
