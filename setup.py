1654ba46ef2aabb80802b8ce7ad0d7a0c16cfc09
Michal Čihař <michal@cihar.com>
2023-09-08 08:24:42 +0200

    version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements.txt") as handle:
 
 setup(
     name="fakturace",
-    version="0.6.1",
+    version="0.7.0",
     author="Michal Čihař",
     author_email="michal@cihar.com",
     description="Simple invoicing and billing tool",
